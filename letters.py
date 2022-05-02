1108e2831e3299f765d0aae0ddf90da1e339ae26
aaronbi <99830616+aaronbi@users.noreply.github.com>
2022-05-01 22:29:33 -0500

    Add files via upload

--- letters.py
+++ letters.py
@@ -2,32 +2,36 @@ from lark import Lark
 import sys
 import re
 
-def eval_tree(t):
+
+def eval_tree(t,offset=0):
     if t.data == "start":
         for x in t.children:
-            eval_tree(x)
+            if x.data == "offset_up":
+                offset = offset + 1
+            elif x.data == "offset_down":
+                offset = offset - 1
+            eval_tree(x, offset)
     elif t.data == "statement":
-        #print("STATEMENT")
-        eval_tree(t.children[0])
+        eval_tree(t.children[0], offset)
     elif t.data == "command":
         if t.children[0].data == "print":
-            eval_print(t.children[1])
+            eval_print(t.children[1:], offset)
         elif t.children[0].data == "add":
             eval_add(t.children[1:])
         elif t.children[0].data == "char_print":
-            eval_char_print(t.children[1:])
+            eval_char_print(t.children[1:], offset)
 
 
-def eval_print(t):
-    print(t.children[1].children[0])
+def eval_print(t,offset):
+    for x in t:
+        if x.data == "arg":
+            print(x.children[1].children[offset])
 
-def eval_char_print(t):
+def eval_char_print(t,offset):
     out = ""
     for x in t:
-        #print(x.data)
         if x.data == "arg":
-            #print(x.children[1].children[0])
-            out = out + x.children[1].children[0][0]
+            out = out + x.children[1].children[offset][0]
     print(out)
 
 def eval_add(t):
